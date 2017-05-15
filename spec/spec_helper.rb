13096168a658045935083f59c0444608eeac5d73
Greg White <gkwhite95@gmail.com>
2017-05-15 08:52:01 -0400

    Set up rubocop and modify spec_helper
    This fixes an issue caused by the spec_helper not being configured to
    load the dummy rails app

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,9 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'pry'
+require File.expand_path("../dummy/config/environment", __FILE__)
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
